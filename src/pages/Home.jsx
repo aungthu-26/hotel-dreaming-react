d6d38b2d732ac5be6baebec7af3584b28a53a892
Dev104 <user@dev104.local>
2025-11-12 14:23:49 +0630

    feat: navbar, hero, guest mode and footer sections add

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "../components/Hero.jsx";
+import FeaturedDestination from "../components/FeaturedDestination.jsx";
+import ExclusiveOffers from "../components/ExclusiveOffers.jsx";
+import Testimonial from "../components/Testimonial.jsx";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <FeaturedDestination />
+      <ExclusiveOffers />
+      <Testimonial />
+    </>
+  );
+};
+
+export default Home;
